bf6817adb78aaa5611a427d66329a2cbd7ab4d12
Seif Ghazi <seif.ghazi@ibm.com>
2022-03-13 15:56:32 -0400

    test PR

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.send("success");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+
+let toDelete;
